0c7278a6936553e17579013b36be0eadc7b40867
marcio heleno <marcio012@edu.unifor.br>
2018-09-30 01:43:30 -0300

    commit initial

---
+++ Trabalho_TecnicasProgramacao_oficina/src/main/java/website/marcioheleno/Trabalho_TecnicasProgramacao_oficina/App.java
@@ -0,0 +1,13 @@
+package website.marcioheleno.Trabalho_TecnicasProgramacao_oficina;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Trabalho_TecnicasProgramacao_oficina/src/test/java/website/marcioheleno/Trabalho_TecnicasProgramacao_oficina/AppTest.java
@@ -0,0 +1,20 @@
+package website.marcioheleno.Trabalho_TecnicasProgramacao_oficina;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
